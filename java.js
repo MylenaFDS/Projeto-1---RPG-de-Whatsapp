72f9d31000494edb27753c5ff70e6483ce902100
Mylena Ferreira de Souza <mylenafds@gmail.com>
2023-09-14 00:53:59 -0300

    Adicionando novas páginas ao projeto

--- java.js
+++ java.js
@@ -1,4 +1,48 @@
-function redirecionarParaPaginaPersonagem() {
+function redirecionarParaPaginaLegolas() {
     window.location.href = "pagina_legolas.html";
+}
+
+function redirecionarParaPaginaMerlin() {
+    window.location.href = "pagina_merlin.html";
+}
+
+function redirecionarParaPaginaLoki() {
+    window.location.href = "pagina_loki.html";
+}
+
+function redirecionarParaPaginaHades() {
+    window.location.href = "pagina_hades.html";
+}
+
+function redirecionarParaPaginaHarry() {
+    window.location.href = "pagina_harry.html";
+}
+
+function redirecionarParaPaginaKatniss() {
+    window.location.href = "pagina_katniss.html";
+}
+
+function redirecionarParaPaginaFrodo() {
+    window.location.href = "pagina_frodo.html";
+}
+
+function redirecionarParaPaginaHermione() {
+    window.location.href = "pagina_hermione.html";
+}
+
+function redirecionarParaPaginaDaemon() {
+    window.location.href = "pagina_daemon.html";
+}
+
+function redirecionarParaPaginaLara() {
+    window.location.href = "pagina_lara.html";
+}
+
+function redirecionarParaPaginaAragorn() {
+    window.location.href = "pagina_aragorn.html";
+}
+
+function redirecionarParaPaginaMulherMaravilha() {
+    window.location.href = "pagina_mulher_maravilha.html";
 }
 
